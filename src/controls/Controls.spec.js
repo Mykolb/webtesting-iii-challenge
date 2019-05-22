d2d38902f3304a46e8859404ec5f419ec4acd5f1
Mykol Benning <mykolbenning@MBP13inhMid2012.fios-router.home>
2019-05-22 18:10:17 -0400

    Display, Control done, added snapshot to dashboard

--- src/controls/Controls.spec.js
+++ src/controls/Controls.spec.js
@@ -1,1 +1,28 @@
 // Test away!
+import React from 'react';
+import { render, fireEvent, cleanup} from 'react-testing-library';
+import 'jest-dom/extend-expect';
+import Controls from './Controls';
+
+
+describe('<Controls />', () => {
+    it('closed toggle button is disabled if the gate is locked', () => {
+
+        //if it's locked and gate is closed the text that should appear is "Open Gate"
+        const mock = jest.fn()
+        const { getByText } = render(<Controls closed={true} locked={true} />)
+        const button = getByText(/Open Gate/i)
+        fireEvent.click(button)
+        expect(mock).not.toHaveBeenCalled();
+    })
+
+    it('locked toggle button is disabled if the gate is open', () => {
+
+        //if it's unlocked and gate is open the text that should appear is "Close Gate"
+        const mock = jest.fn()
+        const { getByText } = render(<Controls closed={false} locked={false} />)
+        const button = getByText(/Close Gate/i)
+        fireEvent.click(button)
+        expect(mock).not.toHaveBeenCalled();
+    })
+})
--- src/dashboard/Dashboard.spec.js
+++ src/dashboard/Dashboard.spec.js
@@ -1,1 +1,21 @@
 // Test away
+import React from 'react';
+import renderer from 'react-test-renderer'; // 1: install this npm module as a dev dependency
+//add library and jest 
+import { render, fireEvent} from 'react-testing-library';
+import 'jest-dom/extend-expect';
+import Dashboard from './Dashboard';
+
+describe('<Dashboard />', () => {
+    // 2. write this test
+    it('matches snapshot', () => {
+      const tree = renderer.create(<Dashboard />); // generates a DOM tree
+  
+      // snapshots are a JSON representation of the DOM tree
+      expect(tree.toJSON()).toMatchSnapshot();
+    });
+  });
+  
+
+
+  export default Dashboard;
